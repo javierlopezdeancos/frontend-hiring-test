ada5abe5c5e8080e4ca8633c8ff6b962d340a240
Javier Lopez de Ancos <javier.lopezdeancos@wayflyer.com>
2023-02-06 13:39:53 +0100

    fix: meoize component

--- phone-test/src/pages/Calls/CallDetails/components/CallNote.tsx
+++ phone-test/src/pages/Calls/CallDetails/components/CallNote.tsx
@@ -1,15 +1,15 @@
-import { FC } from 'react';
+import React, { FC } from 'react';
 import { Spacer, NotesOutlined } from '@aircall/tractor';
 
 interface CallNoteProps {
   content: string;
 }
 
-export const CallNote: FC<CallNoteProps> = ({ content }) => {
+export const CallNote: FC<CallNoteProps> = React.memo(({ content }) => {
   return (
     <Spacer space="s" direction="horizontal" alignItems="center" justifyItems="center">
       <NotesOutlined />
       <span>{content}</span>
     </Spacer>
   );
-};
+});
